97d5f72c6e30f0dccc352dcad4c27e6f645f2f19
liubing <576192031@qq.com>
2018-10-10 21:46:27 +0800

    fix:更新节点内容

--- src/main/java/zookeeper/GetChildNodeData.java
+++ src/main/java/zookeeper/GetChildNodeData.java
@@ -5,12 +5,12 @@ package zookeeper;
  *
  */
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import common.Constant;
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
 public class GetChildNodeData implements Watcher{
@@ -23,11 +23,58 @@ public class GetChildNodeData implements Watcher{
      * 使用同步api获取节点数据内容
      */
     @Test
-    public void getData(){
+    public void getData1() throws IOException, InterruptedException, KeeperException {
+        String path = "/zk-book1";
+        zooKeeper = new ZooKeeper(Constant.connectString ,Constant.sessionTimeOut ,new GetChildrenNode());
+        countDownLatch.await();
+        zooKeeper.create(path ,"123".getBytes() , ZooDefs.Ids.OPEN_ACL_UNSAFE , CreateMode.EPHEMERAL);
+        System.out.println(new String(zooKeeper.getData(path ,true ,stat)));
+        System.out.println(stat.getCzxid()+","+stat.getMzxid()+","+stat.getVersion());
+
+        zooKeeper.setData(path ,"123".getBytes() ,-1);
+        Thread.sleep(Integer.MAX_VALUE);
 
     }
 
-    public void process(WatchedEvent watchedEvent) {
+    /**
+     * 使用异步api获取节点数据内容
+     */
+    @Test
+    public void getData2() throws IOException, InterruptedException, KeeperException {
+        String path = "/zk-book";
+        zooKeeper = new ZooKeeper(Constant.connectString ,Constant.sessionTimeOut ,new GetChildrenNode());
+        countDownLatch.await();
+        zooKeeper.create(path ,"123".getBytes() , ZooDefs.Ids.OPEN_ACL_UNSAFE , CreateMode.EPHEMERAL);
 
+        zooKeeper.getData(path ,true ,new IDataCallback() ,null);
+        zooKeeper.setData(path ,"123".getBytes() ,-1);
+        Thread.sleep(Integer.MAX_VALUE);
+
+    }
+
+
+
+    public void process(WatchedEvent watchedEvent) {
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            countDownLatch.countDown();
+        }else if (watchedEvent.getType() == Event.EventType.NodeDataChanged) {
+            try {
+                System.out.println(new String(zooKeeper.getData(watchedEvent.getPath() ,true ,stat)));
+                System.out.println(stat.getCzxid()+","+stat.getMzxid()+","+stat.getVersion());
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    class IDataCallback implements AsyncCallback.DataCallback{
+
+        public void processResult(int i, String s, Object o, byte[] bytes, Stat stat) {
+            System.out.println(i+","+s+","+new String(bytes));
+            System.out.println(stat.getCzxid()+","+stat.getMzxid()+","+stat.getVersion());
+        }
     }
 }
---
+++ src/main/java/zookeeper/UpdateNodeData.java
@@ -0,0 +1,56 @@
+package zookeeper;
+/*
+ *一句话描述该类作用:【更新节点数据】
+ *@Author:LB
+ *
+ */
+
+import common.Constant;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class UpdateNodeData implements Watcher{
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(1);
+    private static ZooKeeper zooKeeper = null;
+
+    /**
+     * 使用同步api更新节点数据内容
+     */
+    @Test
+    public void updateData1() throws IOException, InterruptedException, KeeperException {
+        String path = "zk-apple";
+        zooKeeper = new ZooKeeper(Constant.connectString ,Constant.sessionTimeOut ,new UpdateNodeData());
+        countDownLatch.await();
+        zooKeeper.create(path ,"123".getBytes() , ZooDefs.Ids.OPEN_ACL_UNSAFE , CreateMode.EPHEMERAL);
+        zooKeeper.getData(path ,true ,null);
+
+        /**
+         * -1表示基于数据的最新版本进行更新
+         */
+        Stat stat = zooKeeper.setData(path ,"456".getBytes() ,-1);
+        System.out.println(stat.getCzxid()+","+stat.getMzxid()+","+stat.getVersion());
+
+        Stat stat1 = zooKeeper.setData(path ,"456".getBytes() ,stat.getVersion());
+        System.out.println(stat1.getCzxid()+","+stat1.getMzxid()+","+stat1.getVersion());
+
+        zooKeeper.setData(path ,"456".getBytes() ,stat.getVersion());
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+    }
+
+
+
+    public void process(WatchedEvent watchedEvent) {
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            if (Event.EventType.None == watchedEvent.getType() && null == watchedEvent.getPath()) {
+                countDownLatch.countDown();
+            }
+        }
+    }
+}
