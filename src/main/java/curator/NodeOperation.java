98eed3e4cc2f8539e6d09befb2da91f0e182b149
liubing <576192031@qq.com>
2018-10-12 23:15:06 +0800

    fix:使用curator客户端进行节点操作

--- src/main/java/curator/NodeOperation.java
+++ src/main/java/curator/NodeOperation.java
@@ -11,6 +11,7 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 import org.junit.jupiter.api.Test;
 
 public class NodeOperation {
@@ -23,12 +24,13 @@ public class NodeOperation {
                                                                             .sessionTimeoutMs(Constant.sessionTimeOut)
                                                                             .retryPolicy(retryPolicy)
                                                                             .build();
-
+/*--------------------------------------------同步接口-------------------------------------------------*/
     /**
      * 节点创建
      */
     @Test
     public void createNode() throws Exception {
+        client.start();
         /*
          * 创建一个节点，初始内容为空
          */
@@ -59,13 +61,60 @@ public class NodeOperation {
      */
     @Test
     public void deleteNode() throws Exception {
+        client.start();
         /*
          * 删除一个节点，只能删除叶子节点
          */
         client.delete().forPath(path);
+        /**
+         * 删除一个节点，并且递归删除所有子节点
+         */
+        client.delete().deletingChildrenIfNeeded().forPath(path);
+        /**
+         * 强制指定版本进行删除
+         */
+        client.delete().withVersion(1).forPath(path);
+        /**
+         * 删除一个节点，强制保证删除(排除网络原因)
+         */
+        client.delete().guaranteed().forPath(path);
+    }
 
-
+    /**
+     * 读取数据
+     */
+    @Test
+    public void getData() throws Exception {
+        client.start();
+        /**
+         * 读取一个节点的数据内容
+         */
+        byte[] bytes = client.getData().forPath(path);
+        /**
+         * 读取一个节点的数据，并且获取到该节点的stat
+         */
+        byte[] bytes1 = client.getData().storingStatIn(new Stat()).forPath(path);
 
     }
+
+    /**
+     * 更新数据
+     */
+    @Test
+    public void updateData() throws Exception {
+        client.start();
+        /**
+         * 更新一个节点的数据内容
+         */
+        Stat stat = client.setData().forPath(path);
+        /**
+         * 强制指定节点更新，withVersion用来实现cas的，version信息是从一个旧的stat对象中获取的
+         */
+        Stat stat1 = client.setData().withVersion(1).forPath(path);
+
+    }
+
+    /*--------------------------------------------异步接口-------------------------------------------------*/
+
 
 }
