cd68fd3664ff6cae1bf5eae5df1cb8ef8613a9b3
liubing <576192031@qq.com>
2018-10-12 23:30:10 +0800

    fix:curator客户端典型使用场景

--- src/main/java/curator/NodeOperation.java
+++ src/main/java/curator/NodeOperation.java
@@ -9,10 +9,16 @@ import common.Constant;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.BackgroundCallback;
+import org.apache.curator.framework.api.CuratorEvent;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NodeOperation {
 
@@ -24,6 +30,9 @@ public class NodeOperation {
                                                                             .sessionTimeoutMs(Constant.sessionTimeOut)
                                                                             .retryPolicy(retryPolicy)
                                                                             .build();
+    private static CountDownLatch semaphore = new CountDownLatch(2);
+
+    private static ExecutorService tp = Executors.newFixedThreadPool(2);
 /*--------------------------------------------同步接口-------------------------------------------------*/
     /**
      * 节点创建
@@ -116,5 +125,32 @@ public class NodeOperation {
 
     /*--------------------------------------------异步接口-------------------------------------------------*/
 
+    /**
+     * 使用异步接口
+     */
+    public void nodeOperation() throws Exception {
+        client.start();
+        System.out.println("Main thread:" + Thread.currentThread().getName());
+        //传入自定义的Executor
+        client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).inBackground(new BackgroundCallback() {
+            public void processResult(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
+                System.out.println("event[code:"+curatorEvent.getResultCode() + ", type: " + curatorEvent.getType() + "]");
+                System.out.println("Thread of processResult:"+Thread.currentThread().getName());
+                semaphore.countDown();
+            }
+        } ,tp).forPath(path ,"init".getBytes());
+
+        //没有传入自定义的Executor
+        client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).inBackground(new BackgroundCallback() {
+            public void processResult(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
+                System.out.println("event[code:"+curatorEvent.getResultCode() + ", type: " + curatorEvent.getType() + "]");
+                System.out.println("Thread of processResult:"+Thread.currentThread().getName());
+                semaphore.countDown();
+            }
+        } ,tp).forPath(path ,"init".getBytes());
+
+        semaphore.await();
+        tp.shutdown();
+    }
 
 }
---
+++ src/main/java/curator/UseExample.java
@@ -0,0 +1,11 @@
+package curator;
+/*
+ *一句话描述该类作用:【典型的使用场景】
+ *@Author:LB
+ *
+ */
+
+public class UseExample {
+
+
+}
