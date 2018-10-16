c6c96aa76e3c94e13666cdeabb385314961c194b
liubing <576192031@qq.com>
2018-10-16 22:31:45 +0800

    fix:dubbo消费者api

--- dubbo-user/src/main/java/main/App.java
+++ dubbo-user/src/main/java/main/App.java
@@ -8,6 +8,18 @@ package main;
 public class App {
 
     public static void main(String[] arges){
-        //
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("order-consumer.xml");
+
+        //用户下单过程
+        IOrderServices services=(IOrderServices)context.getBean("orderServices");
+
+        DoOrderRequest request=new DoOrderRequest();
+        request.setName("mic");
+        DoOrderResponse response=services.doOrder(request);
+
+        System.out.println(response);
+
+        //Order.doOrder();
+        System.in.read();
     }
 }
