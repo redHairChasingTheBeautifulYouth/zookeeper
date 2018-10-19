6b16e81f3da2c6cc108c266150baffc136da578a
liubing <576192031@qq.com>
2018-10-19 23:49:17 +0800

    fix:dubbo provider调用

--- dubbo-user/src/main/java/main/App.java
+++ dubbo-user/src/main/java/main/App.java
@@ -5,13 +5,19 @@ package main;
  *
  */
 
+import zookeeper.DoOrderRequest;
+import zookeeper.DoOrderResponse;
+import zookeeper.IOrderServices;
+
+import java.io.IOException;
+
 public class App {
 
-    public static void main(String[] arges){
-        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("order-consumer.xml");
+    public static void main(String[] arges) throws IOException {
+        //ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("order-consumer.xml");
 
         //用户下单过程
-        IOrderServices services=(IOrderServices)context.getBean("orderServices");
+        IOrderServices services = null;
 
         DoOrderRequest request=new DoOrderRequest();
         request.setName("mic");
